f012a6150911d5d9d1acaf350b77a063ca862a33
DeepLake1 <saawkkaa@gmail.com>
2020-10-11 21:47:10 +0300

    Hw 02 some changes

---
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+public class UsersUtil {
+    public static final List<User> users = Arrays.asList( new User
+               );
+}
