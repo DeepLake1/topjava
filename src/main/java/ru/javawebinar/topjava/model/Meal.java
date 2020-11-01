c8fd52b980dce2cb0ea14261c522c9ab0a9952ce
DeepLake1 <saawkkaa@gmail.com>
2020-11-01 17:20:48 +0300

    Meal model Hibernate impl added JPAMealRepository impl added

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,18 +1,43 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
+@NamedQueries({
+       @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal u WHERE u.id=:id"),
+        @NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m  WHERE m.id=:id"),
+        @NamedQuery(name = Meal.getBetweenHalfOpen, query = "SELECT m FROM Meal m  WHERE m.id=:id AND m.date_time >=:startDateTime AND m.date_time <:endDateTime"),
+        @NamedQuery(name = Meal.GET_ALL, query = "SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+})
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time" }, name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String GET_ALL = "Meal.getAll";
+    public static final String DELETE = "Meal.delete";
+    public static final String GET = "Meal.get";
+    public static final String getBetweenHalfOpen = "Meal.getBetweenHalfOpen";
+    @Column(name = "date_time", nullable = false, unique = true)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @NotBlank
+    @Column(name = "description", nullable = false, unique = true)
+    @Size(max = 400)
     private String description;
 
+
+    @Column(name = "calories", nullable = false, columnDefinition = "int default 500")
+    @Range(min = 10, max = 10000)
     private int calories;
 
+    @JoinColumn(name= "user_id" )
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
